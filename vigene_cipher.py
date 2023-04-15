1153c7dbcd87858788d031dd33b58c5346ea67ab
Vlera Luma <118343087+VleraLuma@users.noreply.github.com>
2023-04-15 17:07:18 +0200

    Update vigene_cipher.py

--- vigene_cipher.py
+++ vigene_cipher.py
@@ -1,18 +1,19 @@
 import hashlib
 
 def generate_keystream(seed, message_length):
+  
 
-if isinstance(seed, str):
-seed=seed.encode('utf-8')
-seed=int.from_bytes(hashlib.sha256(seed).digest(),byteorder='big')
-keystream=[]
-#Per keystream-in e perdorim XOR-shift
-for i in range(message_length):
-  seed^=seed<<13
-  seed^=seed>>17
-  seed^=seed<<5
-  num=seed&0x7fffffff
-  keystream.append(num%26)
+  if isinstance(seed, str):
+      seed=seed.encode('utf-8')
+  seed=int.from_bytes(hashlib.sha256(seed).digest(),byteorder='big')
+  keystream=[]
+  #Per keystream-in e perdorim XOR-shift
+  for i in range(message_length):
+      seed^=seed<<13
+      seed^=seed>>17
+      seed^=seed<<5
+      num=seed&0x7fffffff
+      keystream.append(num%26)
   return keystream
   
   
