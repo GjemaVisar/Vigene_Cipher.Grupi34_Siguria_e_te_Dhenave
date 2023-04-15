68aee649c34c2e9e8868f9f21977a637b1c90a34
Vijola Neziri <116035034+Vijola-Neziri@users.noreply.github.com>
2023-04-15 16:03:24 +0200

    Update vigene_cipher.py

--- vigene_cipher.py
+++ vigene_cipher.py
@@ -1,1 +1,8 @@
 import hashlib
+
+def generate_keystream(seed, message_length):
+
+if isinstance(seed, str):
+seed=seed.encode('utf-8')
+seed=int.from_bytes(hashlib.sha256(seed).digest(),byteorder='big')
+keystream=[]
